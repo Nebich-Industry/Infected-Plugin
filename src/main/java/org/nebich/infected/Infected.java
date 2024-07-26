d9a50ef86b6a5e0ddc38b4150b0c10e90c6c4e92
Nebich <paris.emilien@hotmail.fr>
2024-07-27 00:09:20 +0200

    :sparkles: First commit

---
+++ src/main/java/org/nebich/infected/Infected.java
@@ -0,0 +1,17 @@
+package org.nebich.infected;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Infected extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
