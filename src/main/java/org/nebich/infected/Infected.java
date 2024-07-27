8ac2daf6b54aaf6c6c7de78526b3c6d12a05517e
Nebich <paris.emilien@hotmail.fr>
2024-07-27 03:41:50 +0200

    :construction: Testing world creation and started to create it

--- src/main/java/org/nebich/infected/Infected.java
+++ src/main/java/org/nebich/infected/Infected.java
@@ -1,13 +1,23 @@
 package org.nebich.infected;
 
 import org.bukkit.Bukkit;
+import org.bukkit.World;
+import org.bukkit.WorldCreator;
+import org.bukkit.WorldType;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.logging.Level;
 
 public final class Infected extends JavaPlugin {
 
     @Override
     public void onEnable() {
         Bukkit.getLogger().info("[Infected] Plugin started");
+        Bukkit.getLogger().log(Level.INFO, "[Infected] Plugin data folder path : %s", getDataFolder().getPath());
+        WorldCreator worldCreator = new WorldCreator(getDataFolder().getPath()+"/maps/world_dead_island");
+        worldCreator.environment(World.Environment.NORMAL);
+        worldCreator.type(WorldType.NORMAL);
+        worldCreator.createWorld();
         // Plugin startup logic
     }
 
