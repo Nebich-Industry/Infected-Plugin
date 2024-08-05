834e4a4730a9a4102c65dfb2183901eb4abc8643
Nebich <paris.emilien@hotmail.fr>
2024-08-05 22:49:10 +0200

    :sparkles: Automatically load worlds starting by world_infected

--- src/main/java/org/nebich/infected/worlds/WorldsManager.java
+++ src/main/java/org/nebich/infected/worlds/WorldsManager.java
@@ -1,9 +1,20 @@
 package org.nebich.infected.worlds;
 
+import org.bukkit.Bukkit;
 import org.bukkit.World;
+import org.bukkit.WorldCreator;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
 
 public class WorldsManager {
     private World currentWorld = null;
+    private final Set<World> worlds = new HashSet<>();
 
     public WorldsManager() {
 
@@ -13,7 +24,27 @@ public class WorldsManager {
         return this.currentWorld;
     }
 
-    public void setCurrentWorld(World world) {
+    private void setCurrentWorld(World world) {
         this.currentWorld = world;
+    }
+
+    private List<String> getAllWorlds() {
+        List<String> infectedWorlds = new ArrayList<>();
+        for (File potentialWorld : Objects.requireNonNull(Bukkit.getServer().getWorldContainer().listFiles())) {
+            if (potentialWorld.isDirectory() && Arrays.asList(Objects.requireNonNull(potentialWorld.list())).contains("level.dat") && potentialWorld.getName().startsWith("world_infected")) {
+                infectedWorlds.add(potentialWorld.getName());
+            }
+        }
+        return infectedWorlds;
+    }
+
+    public void loadWorlds() {
+        for (String worldName : this.getAllWorlds()) {
+            WorldCreator worldCreator = new WorldCreator(worldName);
+            World createdWorld = worldCreator.createWorld();
+            this.worlds.add(createdWorld);
+        }
+        this.setCurrentWorld(this.worlds.iterator().next());
+        Bukkit.getLogger().info("[Infected] Loaded world(s) successfully");
     }
 }
